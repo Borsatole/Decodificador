b25943489bbf5be3d22a0b6e2c72f7d93ebffcc5
Borsatole <borsatole@gmail.com>
2024-01-24 17:55:16 -0300

    atualização

---
+++ script/app.js
@@ -0,0 +1,14 @@
+// Challenger Alura!
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "a" é convertida para "ai"
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+let escrita_usuario = document.getElementById('text_area');
+console.log (escrita_usuario.value);
+
+
+
