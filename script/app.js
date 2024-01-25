1582431df3caac0c9176232db1185998af150cd7
Borsatole <borsatole@gmail.com>
2024-01-25 16:31:16 -0300

    25-01

--- script/app.js
+++ script/app.js
@@ -7,8 +7,24 @@
 // A letra "o" é convertida para "ober"
 // A letra "u" é convertida para "ufat"
 
-let escrita_usuario = document.getElementById('text_area');
-console.log (escrita_usuario.value);
+
+
+function criptografar () {
+    
+    let escrita_usuario = document.getElementById('text_area').value;
+    let replaced = escrita_usuario.replace("a","ai").replace("e","enter").replace("i","imes").replace("o","ober").replace("u","ufat");
+
+    escrita_usuario.value = '';
+    console.log (replaced);
+}
+
+function descriptografar () {
+    
+    let escrita_usuario = document.getElementById('text_area').value;
+    let replaced = escrita_usuario.replace("ai","a").replace("enter","e").replace("imes","i").replace("ober","o").replace("ufat","u");
+    
+    console.log (replaced);
+}
 
 
 
