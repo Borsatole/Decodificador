f06efedc8c02cdc807514c44047dd28099292e33
F Santos <borsatole@gmailcom>
2024-02-13 19:20:34 -0300

    criação de modal de erro

--- script/app.js
+++ script/app.js
@@ -1,20 +1,9 @@
-// Challenger Alura!
-
-// As "chaves" de criptografia que utilizaremos são:
-// A letra "a" é convertida para "ai"
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
-
-
-
 function criptografar () {
     
     let textarea = document.getElementById('text_area')
     let escrita_usuario = textarea.value;
 
-    if (escrita_usuario == "") {alert("Não há texto para criptografar")} else {
+    if (escrita_usuario == "") {abrir_modal_erro()} else {
     let criptografado = escrita_usuario.replaceAll("i","imes").replaceAll("a","ai").replaceAll("e","enter").replaceAll("o","ober").replaceAll("u","ufat");
 
     textarea.value = criptografado;
@@ -26,7 +15,7 @@ function descriptografar () {
     let textarea = document.getElementById('text_area');
     let escrita_usuario = textarea.value ;
     
-    if (escrita_usuario == "") {alert("Não há texto para descriptografar")} else {
+    if (escrita_usuario == "") {abrir_modal_erro()} else {
     let descriptografado = escrita_usuario.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
     
     textarea.value = descriptografado;
@@ -36,11 +25,29 @@ function descriptografar () {
 async function copiar () {
     let textarea = document.getElementById('text_area')
     let escrita_usuario = document.getElementById('text_area').value;
-    
+
+    if (escrita_usuario == "") {abrir_modal_erro()} else{
     await navigator.clipboard.writeText(escrita_usuario);
     alert("Texto copiado");
-
 }
+}
+
+var modal = document.getElementById("modal_erro");
+var btn_fechar = document.getElementById("tentenovamente");
+
+btn_fechar.addEventListener("click",fechar_modal_erro)
+
+function fechar_modal_erro() {
+    modal.style.display = "none";
+}
+
+function abrir_modal_erro() {
+    modal.style.display = "flex";
+}
+abrir_modal_erro()
+
+
+
 
 
 
